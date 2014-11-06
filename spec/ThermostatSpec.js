1c529a41c1f6872fe987676de481e7c8ca3a7f63
Thomas Beeley <tbeeley@gmail.com>
2014-11-06 14:57:22 +0000

    increaseTemperature method added

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -8,4 +8,13 @@ describe("Thermostat", function() {
   it("should initialize at 20 degrees", function() {
     expect(thermostat.temperature).toEqual(20)
   });
+
+  describe('increasing the temperature', function() {
+  
+    it("increase if < 25 degrees", function() {
+      thermostat.increaseTemperature()
+      expect(thermostat.temperature).toEqual(21)
+    });
+  
+  });
 });
